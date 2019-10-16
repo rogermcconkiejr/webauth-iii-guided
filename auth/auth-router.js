2eb03761d47cf504b1d3636710e8ca754f9e6398
Roger McConkie <roger.mcconkiejr@gmail.com>
2019-10-16 10:49:58 -0600

    implements web token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -25,8 +26,11 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        //produce token
+        const token = generateToken(user)
+        //add token to response
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          message: `Welcome ${user.username}!`, token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +40,18 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload ={
+    username: user.username,
+    subject: user.id
+  };
+  const secret ='is it secret? is it safe?';
+  const options ={
+    expiresIn: '1h',
+  }
+  return jwt.sign(payload, secret, options)
+}
+
 
 module.exports = router;
